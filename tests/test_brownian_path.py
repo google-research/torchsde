2e02297ac318f7684e91b880b70fc2cc390d39a2
Xuechen <12689993+lxuechen@users.noreply.github.com>
2020-08-12 00:28:04 -0400

    Simplify tests with pytest.

--- tests/test_brownian_path.py
+++ tests/test_brownian_path.py
@@ -12,6 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""Test the two `BrownianPath`.
+
+The suite tests both running on CPU and CUDA (if available).
+"""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -20,16 +24,15 @@ import sys
 
 sys.path = sys.path[1:]  # A hack so that we always import the installed library.
 
-import unittest
-
 import numpy as np
 import numpy.random as npr
 import torch
 from scipy.stats import norm, kstest
 
-from tests.torch_test import TorchTestCase
-from torchsde.brownian_lib import BrownianPath
+import itertools
 
+import torchsde
+import pytest
 
 torch.manual_seed(0)
 torch.set_default_dtype(torch.float64)
@@ -39,60 +42,62 @@ BATCH_SIZE = 131072
 REPS = 3
 ALPHA = 0.001
 
-
-class TestBrownianPath(TorchTestCase):
-
-    def _setUp(self, device=None):
-        t0, t1 = torch.tensor([0., 1.]).to(device)
-        w0, w1 = torch.randn([2, BATCH_SIZE, D]).to(device)
-
-        self.t = torch.rand([]).to(device)
-        self.bm = BrownianPath(t0=t0, w0=w0)
-
-    def test_basic_cpu(self):
-        self._setUp(device=torch.device('cpu'))
-        sample = self.bm(self.t)
-        self.assertEqual(sample.size(), (BATCH_SIZE, D))
-
-    def test_basic_gpu(self):
-        if not torch.cuda.is_available():
-            self.skipTest(reason='CUDA not available.')
-
-        self._setUp(device=torch.device('cuda'))
-        sample = self.bm(self.t)
-        self.assertEqual(sample.size(), (BATCH_SIZE, D))
-
-    def test_determinism(self):
-        self._setUp()
-        vals = [self.bm(self.t) for _ in range(REPS)]
-        for val in vals[1:]:
-            self.tensorAssertAllClose(val, vals[0])
-
-    def test_normality(self):
-        """Kolmogorov-Smirnov test."""
-        t0_, t1_ = 0.0, 1.0
-        t0, t1 = torch.tensor([t0_, t1_])
-        eps = 1e-2
-        for _ in range(REPS):
-            w0_, w1_ = 0.0, npr.randn() * np.sqrt(t1_)
-            # Use the same endpoint for the batch, so samples from same dist.
-            w0 = torch.tensor(w0_).repeat(BATCH_SIZE)
-            w1 = torch.tensor(w1_).repeat(BATCH_SIZE)
-
-            bm = BrownianPath(t0=t0, w0=w0)
-            bm.insert(t=t1, w=w1)
-
-            t_ = npr.uniform(low=t0_ + eps, high=t1_ - eps)
-            samples = bm(t_)
-            samples_ = samples.detach().numpy()
-
-            mean_ = ((t1_ - t_) * w0_ + (t_ - t0_) * w1_) / (t1_ - t0_)
-            std_ = np.sqrt((t1_ - t_) * (t_ - t0_) / (t1_ - t0_))
-            ref_dist = norm(loc=mean_, scale=std_)
-
-            _, pval = kstest(samples_, ref_dist.cdf)
-            self.assertGreaterEqual(pval, ALPHA)
+brownian_classes = [torchsde.brownian_lib.BrownianPath, torchsde.BrownianPath]
+devices = [cpu, gpu] = [torch.device('cpu'), torch.device('cuda')]
 
 
-if __name__ == '__main__':
-    unittest.main()
+def _setup(brownian_class, device):
+    t0, t1 = torch.tensor([0., 1.]).to(device)
+    w0, w1 = torch.randn([2, BATCH_SIZE, D]).to(device)
+    t = torch.rand([]).to(device)
+    bm = brownian_class(t0=t0, w0=w0)
+    return t, bm
+
+
+@pytest.mark.parametrize("brownian_class, device", itertools.product(brownian_classes, devices))
+def test_basic(brownian_class, device):
+    if device == gpu and not torch.cuda.is_available():
+        pytest.skip(msg="CUDA not available.")
+
+    t, bm = _setup(brownian_class, device)
+    sample = bm(t)
+    assert sample.size() == (BATCH_SIZE, D)
+
+
+@pytest.mark.parametrize("brownian_class, device", itertools.product(brownian_classes, devices))
+def test_determinism(brownian_class, device):
+    if device == gpu and not torch.cuda.is_available():
+        pytest.skip(msg="CUDA not available.")
+
+    t, bm = _setup(brownian_class, device)
+    vals = [bm(t) for _ in range(REPS)]
+    for val in vals[1:]:
+        assert torch.allclose(val, vals[0])
+
+
+@pytest.mark.parametrize("brownian_class, device", itertools.product(brownian_classes, devices))
+def test_normality(brownian_class, device):
+    if device == gpu and not torch.cuda.is_available():
+        pytest.skip(msg="CUDA not available.")
+
+    t0_, t1_ = 0.0, 1.0
+    t0, t1 = torch.tensor([t0_, t1_]).to(device)
+    eps = 1e-2
+    for _ in range(REPS):
+        w0_, w1_ = 0.0, npr.randn() * np.sqrt(t1_)
+        w0 = torch.tensor(w0_).repeat(BATCH_SIZE).to(device)
+        w1 = torch.tensor(w1_).repeat(BATCH_SIZE).to(device)
+
+        bm = brownian_class(t0=t0, w0=w0)  # noqa
+        bm.insert(t=t1, w=w1)
+
+        t_ = npr.uniform(low=t0_ + eps, high=t1_ - eps)
+        samples = bm(t_)
+        samples_ = samples.cpu().detach().numpy()
+
+        mean_ = ((t1_ - t_) * w0_ + (t_ - t0_) * w1_) / (t1_ - t0_)
+        std_ = np.sqrt((t1_ - t_) * (t_ - t0_) / (t1_ - t0_))
+        ref_dist = norm(loc=mean_, scale=std_)
+
+        _, pval = kstest(samples_, ref_dist.cdf)
+        assert pval >= ALPHA
--- tests/test_brownian_tree.py
+++ tests/test_brownian_tree.py
@@ -12,6 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""Test the two `BrownianPath`.
+
+The suite tests both running on CPU and CUDA (if available).
+"""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -20,15 +24,14 @@ import sys
 
 sys.path = sys.path[1:]  # A hack so that we always import the installed library.
 
-import unittest
-
 import numpy as np
 import numpy.random as npr
 import torch
 from scipy.stats import norm, kstest
 
-from tests.torch_test import TorchTestCase
-from torchsde.brownian_lib import BrownianTree
+import itertools
+import pytest
+import torchsde
 
 torch.manual_seed(0)
 torch.set_default_dtype(torch.float64)
@@ -39,84 +42,16 @@ LARGE_BATCH_SIZE = 16384
 REPS = 3
 ALPHA = 0.001
 
+brownian_classes = [torchsde.brownian_lib.BrownianTree, torchsde.BrownianTree]
+devices = [cpu, gpu] = [torch.device('cpu'), torch.device('cuda')]
 
-class TestBrownianTree(TorchTestCase):
 
-    def _setUp(self, batch_size, device=None):
-        t0, t1 = torch.tensor([0., 1.]).to(device)
-        w0 = torch.zeros(batch_size, D).to(device=device)
-        t = torch.rand([]).to(device)
-
-        self.t = t
-        self.bm = BrownianTree(t0=t0, t1=t1, w0=w0, entropy=0)
-
-    def test_basic_cpu(self):
-        self._setUp(batch_size=SMALL_BATCH_SIZE, device=torch.device('cpu'))
-        sample = self.bm(self.t)
-        self.assertEqual(sample.size(), (SMALL_BATCH_SIZE, D))
-
-    def test_basic_gpu(self):
-        if not torch.cuda.is_available():
-            self.skipTest(reason='CUDA not available.')
-
-        self._setUp(batch_size=SMALL_BATCH_SIZE, device=torch.device('cuda'))
-        sample = self.bm(self.t)
-        self.assertEqual(sample.size(), (SMALL_BATCH_SIZE, D))
-
-    def test_determinism(self):
-        self._setUp(batch_size=SMALL_BATCH_SIZE)
-        vals = [self.bm(self.t) for _ in range(REPS)]
-        for val in vals[1:]:
-            self.tensorAssertAllClose(val, vals[0])
-
-    def test_normality(self):
-        """Kolmogorov-Smirnov test."""
-        t0_, t1_ = 0.0, 1.0
-        t0, t1 = torch.tensor([t0_, t1_])
-        eps = 1e-5
-        for _ in range(REPS):
-            w0_, w1_ = 0.0, npr.randn()
-            # Use the same endpoint for the batch, so samples from same dist.
-            w0 = torch.tensor(w0_).repeat(LARGE_BATCH_SIZE)
-            w1 = torch.tensor(w1_).repeat(LARGE_BATCH_SIZE)
-            bm = BrownianTree(t0=t0, t1=t1, w0=w0, w1=w1, pool_size=100, tol=1e-14)
-
-            for _ in range(REPS):
-                t_ = npr.uniform(low=t0_ + eps, high=t1_ - eps)
-                samples = bm(t_)
-                samples_ = samples.detach().numpy()
-
-                mean_ = ((t1_ - t_) * w0_ + (t_ - t0_) * w1_) / (t1_ - t0_)
-                std_ = np.sqrt((t1_ - t_) * (t_ - t0_) / (t1_ - t0_))
-                ref_dist = norm(loc=mean_, scale=std_)
-
-                _, pval = kstest(samples_, ref_dist.cdf)
-                self.assertGreaterEqual(pval, ALPHA)
-
-    def test_to_device(self):
-        if not torch.cuda.is_available():
-            self.skipTest(reason='CUDA not available.')
-
-        self._setUp(batch_size=SMALL_BATCH_SIZE)
-        curr, prev, post = _dict_to_sorted_list(*self.bm.get_cache())
-        old = torch.cat(curr + prev + post, dim=0)
-
-        self.bm.to(torch.device('cuda'))
-        curr, prev, post = _dict_to_sorted_list(*self.bm.get_cache())
-        new = torch.cat(curr + prev + post, dim=0)
-        self.assertTrue(str(new.device).startswith('cuda'))
-        self.tensorAssertAllClose(old, new.cpu())
-
-    def test_to_float32(self):
-        self._setUp(batch_size=SMALL_BATCH_SIZE)
-        curr, prev, post = _dict_to_sorted_list(*self.bm.get_cache())
-        old = torch.cat(curr + prev + post, dim=0)
-
-        self.bm.to(torch.float32)
-        curr, prev, post = _dict_to_sorted_list(*self.bm.get_cache())
-        new = torch.cat(curr + prev + post, dim=0)
-        self.assertTrue(new.dtype, torch.float32)
-        self.tensorAssertAllClose(old, new.double())
+def _setup(brownian_class, device, batch_size):
+    t0, t1 = torch.tensor([0., 1.]).to(device)
+    w0 = torch.zeros(batch_size, D).to(device)
+    t = torch.rand([]).to(device)
+    bm = brownian_class(t0=t0, t1=t1, w0=w0, entropy=0)
+    return t, bm
 
 
 def _dict_to_sorted_list(*dicts):
@@ -126,5 +61,81 @@ def _dict_to_sorted_list(*dicts):
     return lists
 
 
-if __name__ == '__main__':
-    unittest.main()
+@pytest.mark.parametrize("brownian_class, device", itertools.product(brownian_classes, devices))
+def test_basic(brownian_class, device):
+    if device == gpu and not torch.cuda.is_available():
+        pytest.skip(msg="CUDA not available.")
+
+    t, bm = _setup(brownian_class, device, SMALL_BATCH_SIZE)
+    sample = bm(t)
+    assert sample.size() == (SMALL_BATCH_SIZE, D)
+
+
+@pytest.mark.parametrize("brownian_class, device", itertools.product(brownian_classes, devices))
+def test_determinism(brownian_class, device):
+    if device == gpu and not torch.cuda.is_available():
+        pytest.skip(msg="CUDA not available.")
+
+    t, bm = _setup(brownian_class, device, SMALL_BATCH_SIZE)
+    vals = [bm(t) for _ in range(REPS)]
+    for val in vals[1:]:
+        assert torch.allclose(val, vals[0])
+
+
+@pytest.mark.parametrize("brownian_class, device", itertools.product(brownian_classes, devices))
+def test_normality(brownian_class, device):
+    if device == gpu and not torch.cuda.is_available():
+        pytest.skip(msg="CUDA not available.")
+
+    t0_, t1_ = 0.0, 1.0
+    t0, t1 = torch.tensor([t0_, t1_]).to(device)
+    eps = 1e-5
+    for _ in range(REPS):
+        w0_, w1_ = 0.0, npr.randn()
+        w0 = torch.tensor(w0_).repeat(LARGE_BATCH_SIZE).to(device)
+        w1 = torch.tensor(w1_).repeat(LARGE_BATCH_SIZE).to(device)
+        bm = brownian_class(t0=t0, t1=t1, w0=w0, w1=w1, pool_size=100, tol=1e-14)  # noqa
+
+        for _ in range(REPS):
+            t_ = npr.uniform(low=t0_ + eps, high=t1_ - eps)
+            samples = bm(t_)
+            samples_ = samples.cpu().detach().numpy()
+
+            mean_ = ((t1_ - t_) * w0_ + (t_ - t0_) * w1_) / (t1_ - t0_)
+            std_ = np.sqrt((t1_ - t_) * (t_ - t0_) / (t1_ - t0_))
+            ref_dist = norm(loc=mean_, scale=std_)
+
+            _, pval = kstest(samples_, ref_dist.cdf)
+            assert pval >= ALPHA
+
+
+@pytest.mark.parametrize("brownian_class, device", itertools.product(brownian_classes, devices))
+def test_to_float32(brownian_class, device):
+    if device == gpu and not torch.cuda.is_available():
+        pytest.skip(msg="CUDA not available.")
+
+    t, bm = _setup(brownian_class, device, SMALL_BATCH_SIZE)
+    curr, prev, post = _dict_to_sorted_list(*bm.get_cache())
+    old = torch.cat(curr + prev + post, dim=0)
+
+    bm.to(torch.float32)
+    curr, prev, post = _dict_to_sorted_list(*bm.get_cache())
+    new = torch.cat(curr + prev + post, dim=0)
+    assert new.dtype == torch.float32
+    assert torch.allclose(old, new.double())
+
+
+@pytest.mark.parametrize("brownian_class", brownian_classes)
+def test_to_device(brownian_class):
+    if not torch.cuda.is_available():
+        pytest.skip(msg="CUDA not available.")
+
+    t, bm = _setup(brownian_class, cpu, SMALL_BATCH_SIZE)
+    curr, prev, post = _dict_to_sorted_list(*bm.get_cache())
+    old = torch.cat(curr + prev + post, dim=0)
+
+    bm.to(gpu)
+    curr, prev, post = _dict_to_sorted_list(*bm.get_cache())
+    new = torch.cat(curr + prev + post, dim=0)
+    assert str(new.device).startswith('cuda')
+    assert torch.allclose(old, new.cpu())
