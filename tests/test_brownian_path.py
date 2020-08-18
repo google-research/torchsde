81202e44ece6f299c016609ad1ae5f233c622fbb
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2020-08-17 22:21:20 -0400

    Refactor tests for Brownian path and Brownian tree.

--- tests/test_brownian_path.py
+++ tests/test_brownian_path.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Test the two `BrownianPath`.
+"""Test `BrownianPath`.
 
 The suite tests both running on CPU and CUDA (if available).
 """
@@ -20,7 +20,7 @@ import sys
 
 sys.path = sys.path[1:]  # A hack so that we always import the installed library.
 
-import numpy as np
+import math
 import numpy.random as npr
 import torch
 from scipy.stats import norm, kstest
@@ -43,9 +43,9 @@ devices = [cpu, gpu] = [torch.device('cpu'), torch.device('cuda')]
 
 
 def _setup(brownian_class, device):
-    t0, t1 = torch.tensor([0., 1.]).to(device)
-    w0, w1 = torch.randn([2, BATCH_SIZE, D]).to(device)
-    t = torch.rand([]).to(device)
+    t0, t1 = torch.tensor([0., 1.], device=device)
+    w0, w1 = torch.randn([2, BATCH_SIZE, D], device=device)
+    t = torch.rand([], device=device)
     bm = brownian_class(t0=t0, w0=w0)
     return t, bm
 
@@ -71,28 +71,30 @@ def test_determinism(brownian_class, device):
         assert torch.allclose(val, vals[0])
 
 
-@pytest.mark.parametrize("brownian_class, device", itertools.product(brownian_classes, devices))
+@pytest.mark.parametrize("brownian_class", brownian_classes)
+@pytest.mark.parametrize("device", devices)
 def test_normality(brownian_class, device):
     if device == gpu and not torch.cuda.is_available():
         pytest.skip(msg="CUDA not available.")
 
     t0_, t1_ = 0.0, 1.0
-    t0, t1 = torch.tensor([t0_, t1_]).to(device)
+    t0, t1 = torch.tensor([t0_, t1_], device=device)
     eps = 1e-2
     for _ in range(REPS):
-        w0_, w1_ = 0.0, npr.randn() * np.sqrt(t1_)
-        w0 = torch.tensor(w0_).repeat(BATCH_SIZE).to(device)
-        w1 = torch.tensor(w1_).repeat(BATCH_SIZE).to(device)
+        w0_, w1_ = 0.0, npr.randn() * math.sqrt(t1_)
+        w0 = torch.tensor(w0_, device=device).repeat(BATCH_SIZE)
+        w1 = torch.tensor(w1_, device=device).repeat(BATCH_SIZE)
 
         bm = brownian_class(t0=t0, w0=w0)  # noqa
         bm.insert(t=t1, w=w1)
 
-        t_ = npr.uniform(low=t0_ + eps, high=t1_ - eps)
+        t_ = npr.uniform(low=t0_ + eps, high=t1_ - eps)  # Avoid sampling too close to the boundary.
         samples = bm(t_)
         samples_ = samples.cpu().detach().numpy()
 
+        # True expected mean from Brownian bridge.
         mean_ = ((t1_ - t_) * w0_ + (t_ - t0_) * w1_) / (t1_ - t0_)
-        std_ = np.sqrt((t1_ - t_) * (t_ - t0_) / (t1_ - t0_))
+        std_ = math.sqrt((t1_ - t_) * (t_ - t0_) / (t1_ - t0_))
         ref_dist = norm(loc=mean_, scale=std_)
 
         _, pval = kstest(samples_, ref_dist.cdf)
