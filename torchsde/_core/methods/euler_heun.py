da9803985918df71237acb7a6ef428ce0cec1f27
Mateusz Sokół <mat646@gmail.com>
2020-08-24 19:51:11 +0200

    Implemented euler-heun

---
+++ torchsde/_core/methods/euler_heun.py
@@ -0,0 +1,45 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from .. import base_solver
+from ...settings import SDE_TYPES, NOISE_TYPES, LEVY_AREA_APPROXIMATIONS
+
+
+class EulerHeun(base_solver.BaseSDESolver):
+    weak_order = 1.0
+    sde_type = SDE_TYPES.stratonovich
+    noise_types = (NOISE_TYPES.additive, NOISE_TYPES.diagonal, NOISE_TYPES.general, NOISE_TYPES.scalar)
+    levy_area_approximations = LEVY_AREA_APPROXIMATIONS.all()
+
+    def __init__(self, sde, **kwargs):
+        if sde.noise_type == NOISE_TYPES.additive:
+            self.strong_order = 0.5
+        else:
+            self.strong_order = 1.0
+        super(EulerHeun, self).__init__(sde=sde, **kwargs)
+
+    def step(self, t0, t1, y0):
+        dt = t1 - t0
+        I_k = self.bm(t0, t1)
+
+        f_eval = self.sde.f(t0, y0)
+        g_prod_eval = self.sde.g_prod(t0, y0, I_k)
+
+        y0_prime = y0 + g_prod_eval        
+
+        g_prod_eval_prime = self.sde.g_prod(t1, y0_prime, I_k)
+
+        y1 = y0 + dt * f_eval + (g_prod_eval + g_prod_eval_prime) * 0.5        
+
+        return y1
