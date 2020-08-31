6692b562b7d205793ec964ddc86c8e99912a7bc0
Mateusz Sokół <mat646@gmail.com>
2020-09-01 00:16:12 +0200

    Formatting

--- diagnostics/stratonovich_diagonal.py
+++ diagnostics/stratonovich_diagonal.py
@@ -43,33 +43,34 @@ def inspect_sample():
         ys_heun = sdeint(sde, y0=y0, ts=ts, dt=dt, bm=bm, method='heun', names={'drift': 'f_corr'})
         ys_euler_heun = sdeint(sde, y0=y0, ts=ts, dt=dt, bm=bm, method='euler_heun', names={'drift': 'f_corr'})
         ys_midpoint = sdeint(sde, y0=y0, ts=ts, dt=dt, bm=bm, method='midpoint', names={'drift': 'f_corr'})
-        ys_milstein_strat = sdeint(sde, y0=y0, ts=ts, dt=dt, bm=bm, method='milstein', names={'drift': 'f_corr'})
-        ys_mil_strat_grad_free = sdeint(sde, y0=y0, ts=ts, dt=dt, bm=bm, method='milstein', names={'drift': 'f_corr'},
+        ys_milstein = sdeint(sde, y0=y0, ts=ts, dt=dt, bm=bm, method='milstein', names={'drift': 'f_corr'})
+        ys_milstein_grad_free = sdeint(sde, y0=y0, ts=ts, dt=dt, bm=bm, method='milstein', names={'drift': 'f_corr'},
                                         options={'grad_free': True})
         ys_analytical = sde.analytical_sample(y0=y0, ts=ts, bm=bm)
 
         ys_heun = ys_heun.squeeze().t()
         ys_euler_heun = ys_euler_heun.squeeze().t()
         ys_midpoint = ys_midpoint.squeeze().t()
-        ys_milstein_strat = ys_milstein_strat.squeeze().t()
-        ys_mil_strat_grad_free = ys_mil_strat_grad_free.squeeze().t()
+        ys_milstein = ys_milstein.squeeze().t()
+        ys_milstein_grad_free = ys_milstein_grad_free.squeeze().t()
         ys_analytical = ys_analytical.squeeze().t()
 
-        ts_, ys_heun_, ys_euler_heun_, ys_midpoint_, ys_milstein_strat_, ys_mil_strat_grad_free_, ys_analytical_ = to_numpy(
-            ts, ys_heun, ys_euler_heun, ys_midpoint, ys_milstein_strat, ys_mil_strat_grad_free, ys_analytical)
+        ts_, ys_heun_, ys_euler_heun_, ys_midpoint_, ys_milstein_, ys_milstein_grad_free_, ys_analytical_ = to_numpy(
+            ts, ys_heun, ys_euler_heun, ys_midpoint, ys_milstein, ys_milstein_grad_free, ys_analytical)
 
     # Visualize sample path.
     img_dir = os.path.join('.', 'diagnostics', 'plots', 'stratonovich_diagonal')
     makedirs_if_not_found(img_dir)
 
-    for i, (ys_heun_i, ys_euler_heun_i, ys_midpoint_i, ys_milstein_strat_i, ys_mil_strat_grad_free_i, ys_analytical_i) in enumerate(
-            zip(ys_heun_, ys_euler_heun_, ys_midpoint_, ys_milstein_strat_, ys_mil_strat_grad_free_, ys_analytical_)):
+    for i, (ys_heun_i, ys_euler_heun_i, ys_midpoint_i, ys_milstein_i, ys_milstein_grad_free_i, ys_analytical_i) \
+            in enumerate(
+            zip(ys_heun_, ys_euler_heun_, ys_midpoint_, ys_milstein_, ys_milstein_grad_free_, ys_analytical_)):
         plt.figure()
         plt.plot(ts_, ys_heun_i, label='heun')
         plt.plot(ts_, ys_euler_heun_i, label='euler_heun')
         plt.plot(ts_, ys_midpoint_i, label='midpoint')
-        plt.plot(ts_, ys_milstein_strat_i, label='milstein_strat')
-        plt.plot(ts_, ys_mil_strat_grad_free_i, label='milstein_strat_grad_free')
+        plt.plot(ts_, ys_milstein_i, label='milstein_strat')
+        plt.plot(ts_, ys_milstein_grad_free_i, label='milstein_strat_grad_free')
         plt.plot(ts_, ys_analytical_i, label='analytical')
         plt.legend()
         plt.savefig(os.path.join(img_dir, f'{i}'))
@@ -86,8 +87,8 @@ def inspect_strong_order():
     heun_mses_ = []
     euler_heun_mses_ = []
     midpoint_mses_ = []
-    milstein_strat_mses_ = []
-    mil_strat_grad_free_mses_ = []
+    milstein_mses_ = []
+    milstein_grad_free_mses_ = []
 
     with torch.no_grad():
         bm = BrownianInterval(t0=ts[0], t1=ts[-1], shape=y0.shape, dtype=y0.dtype, device=device,
@@ -98,46 +99,46 @@ def inspect_strong_order():
             _, ys_heun = sdeint(sde, y0=y0, ts=ts, dt=dt, bm=bm, method='heun', names={'drift': 'f_corr'})
             _, ys_euler_heun = sdeint(sde, y0=y0, ts=ts, dt=dt, bm=bm, method='euler_heun', names={'drift': 'f_corr'})
             _, ys_midpoint = sdeint(sde, y0=y0, ts=ts, dt=dt, bm=bm, method='midpoint', names={'drift': 'f_corr'})
-            _, ys_milstein_strat = sdeint(sde, y0=y0, ts=ts, dt=dt, bm=bm, method='milstein', names={'drift': 'f_corr'})
-            _, ys_mil_strat_grad_free = sdeint(sde, y0=y0, ts=ts, dt=dt, bm=bm, method='milstein',
+            _, ys_milstein = sdeint(sde, y0=y0, ts=ts, dt=dt, bm=bm, method='milstein', names={'drift': 'f_corr'})
+            _, ys_milstein_grad_free = sdeint(sde, y0=y0, ts=ts, dt=dt, bm=bm, method='milstein',
                                                names={'drift': 'f_corr'}, options={'grad_free': True})
             _, ys_analytical = sde.analytical_sample(y0=y0, ts=ts, bm=bm)
 
             heun_mse = compute_mse(ys_heun, ys_analytical)
             euler_heun_mse = compute_mse(ys_euler_heun, ys_analytical)
             midpoint_mse = compute_mse(ys_midpoint, ys_analytical)
-            milstein_strat_mse = compute_mse(ys_milstein_strat, ys_analytical)
-            mil_strat_grad_free_mse = compute_mse(ys_mil_strat_grad_free, ys_analytical)
+            milstein_mse = compute_mse(ys_milstein, ys_analytical)
+            milstein_grad_free_mse = compute_mse(ys_milstein_grad_free, ys_analytical)
 
-            heun_mse_, euler_heun_mse_, midpoint_mse_, milstein_strat_mse_, mil_strat_grad_free_mse_ = to_numpy(
+            heun_mse_, euler_heun_mse_, midpoint_mse_, milstein_mse_, milstein_grad_free_mse_ = to_numpy(
                 heun_mse, 
                 euler_heun_mse, 
                 midpoint_mse,
-                milstein_strat_mse,
-                mil_strat_grad_free_mse
+                milstein_mse,
+                milstein_grad_free_mse
             )
 
             heun_mses_.append(heun_mse_)
             euler_heun_mses_.append(euler_heun_mse_)
             midpoint_mses_.append(midpoint_mse_)
-            milstein_strat_mses_.append(milstein_strat_mse_)
-            mil_strat_grad_free_mses_.append(mil_strat_grad_free_mse_)
-    del heun_mse_, euler_heun_mse_, midpoint_mse_, milstein_strat_mse_, mil_strat_grad_free_mse_
+            milstein_mses_.append(milstein_mse_)
+            milstein_grad_free_mses_.append(milstein_grad_free_mse_)
+    del heun_mse_, euler_heun_mse_, midpoint_mse_, milstein_mse_, milstein_grad_free_mse_
 
     # Divide the log-error by 2, since textbook strong orders are represented so.
     log = lambda x: np.log(np.array(x))
     heun_slope, _, _, _, _ = stats.linregress(log(dts), log(heun_mses_) / 2)
     euler_heun_slope, _, _, _, _ = stats.linregress(log(dts), log(euler_heun_mses_) / 2)
     midpoint_slope, _, _, _, _ = stats.linregress(log(dts), log(midpoint_mses_) / 2)
-    milstein_strat_slope, _, _, _, _ = stats.linregress(log(dts), log(milstein_strat_mses_) / 2)
-    mil_strat_grad_free_slope, _, _, _, _ = stats.linregress(log(dts), log(mil_strat_grad_free_mses_) / 2)
+    milstein_slope, _, _, _, _ = stats.linregress(log(dts), log(milstein_mses_) / 2)
+    milstein_grad_free_slope, _, _, _, _ = stats.linregress(log(dts), log(milstein_grad_free_mses_) / 2)
 
     plt.figure()
     plt.plot(dts, heun_mses_, label=f'heun(k={heun_slope:.4f})')
     plt.plot(dts, euler_heun_mses_, label=f'euler_heun(k={euler_heun_slope:.4f})')
     plt.plot(dts, midpoint_mses_, label=f'midpoint(k={midpoint_slope:.4f})')
-    plt.plot(dts, milstein_strat_mses_, label=f'milstein_strat(k={milstein_strat_slope:.4f})')
-    plt.plot(dts, mil_strat_grad_free_mses_, label=f'milstein_strat_grad_free(k={mil_strat_grad_free_slope:.4f})')
+    plt.plot(dts, milstein_mses_, label=f'milstein_strat(k={milstein_slope:.4f})')
+    plt.plot(dts, milstein_grad_free_mses_, label=f'milstein_strat_grad_free(k={milstein_grad_free_slope:.4f})')
     plt.xscale('log')
     plt.yscale('log')
     plt.legend()
