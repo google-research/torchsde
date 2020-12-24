c233fe6a5408b83afedc39bd3401bf3d64159e60
Patrick Kidger <33688385+patrick-kidger@users.noreply.github.com>
2020-12-24 03:39:53 +0000

    switched all diagnostics to neural network problems, which don't exhibit structure that could get erroneously good convergence orders

--- tests/problems.py
+++ tests/problems.py
@@ -16,13 +16,7 @@
 
 Each example is of a particular noise type.
 
-Ex1, Ex2, Ex3 from
-
-Rackauckas, Christopher, and Qing Nie. "Adaptive methods for stochastic
-differential equations via natural embeddings and rejection sampling with memory."
-Discrete and continuous dynamical systems. Series B 22.7 (2017): 2731.
-
-Ex4 is constructed to test schemes for SDEs with general noise and neural nets.
+Ex1-4 all use simple neural networks.
 
 BasicSDE1-4 are problems where the drift and diffusion may not depend on
 trainable parameters.
@@ -42,35 +36,29 @@ class Ex1(BaseSDE):
 
     def __init__(self, d, sde_type=SDE_TYPES.ito, **kwargs):
         super(Ex1, self).__init__(sde_type=sde_type, noise_type=Ex1.noise_type)
-        self._nfe = 0
 
-        # Use non-exploding initialization.
-        sigma = torch.sigmoid(torch.randn(d))
-        mu = -sigma ** 2 - torch.sigmoid(torch.randn(d))
-        self.mu = nn.Parameter(mu, requires_grad=True)
-        self.sigma = nn.Parameter(sigma, requires_grad=True)
+        self.f_net = nn.Sequential(
+            nn.Linear(d + 1, 3),
+            nn.Softplus(),
+            nn.Linear(3, d)
+        )
+        self.g_net = nn.Sequential(
+            nn.Linear(d + 1, 3),
+            nn.Softplus(),
+            nn.Linear(3, d),
+            nn.Sigmoid()
+        )
 
-        self.f = self.f_ito if sde_type == SDE_TYPES.ito else self.f_stratonovich
-
-    def f_ito(self, t, y):
-        self._nfe += 1
-        return self.mu * y
-
-    def f_stratonovich(self, t, y):
-        self._nfe += 1
-        return self.mu * y - .5 * (self.sigma ** 2) * y
+    def f(self, t, y):
+        ty = torch.cat([t.expand(y.size(0), 1), y], dim=1)
+        return self.f_net(ty)
 
     def g(self, t, y):
-        self._nfe += 1
-        return self.sigma * y
+        ty = torch.cat([t.expand(y.size(0), 1), y], dim=1)
+        return self.g_net(ty)
 
     def h(self, t, y):
-        self._nfe += 1
         return torch.zeros_like(y)
-
-    @property
-    def nfe(self):
-        return self._nfe
 
 
 class Ex2(BaseSDE):
@@ -78,30 +66,29 @@ class Ex2(BaseSDE):
 
     def __init__(self, d, sde_type=SDE_TYPES.ito, **kwargs):
         super(Ex2, self).__init__(sde_type=sde_type, noise_type=Ex2.noise_type)
-        self._nfe = 0
-        self.p = nn.Parameter(torch.sigmoid(torch.randn(d)), requires_grad=True)
 
-        self.f = self.f_ito if sde_type == SDE_TYPES.ito else self.f_stratonovich
+        self.f_net = nn.Sequential(
+            nn.Linear(d + 1, 3),
+            nn.Softplus(),
+            nn.Linear(3, d)
+        )
+        self.g_net = nn.Sequential(
+            nn.Linear(d + 1, 3),
+            nn.Softplus(),
+            nn.Linear(3, d),
+            nn.Sigmoid()
+        )
 
-    def f_ito(self, t, y):
-        self._nfe += 1
-        return -self.p ** 2. * torch.sin(y) * torch.cos(y) ** 3.
-
-    def f_stratonovich(self, t, y):
-        self._nfe += 1
-        return torch.zeros_like(y)
+    def f(self, t, y):
+        ty = torch.cat([t.expand(y.size(0), 1), y], dim=1)
+        return self.f_net(ty)
 
     def g(self, t, y):
-        self._nfe += 1
-        return (self.p * torch.cos(y) ** 2).unsqueeze(dim=-1)
+        ty = torch.cat([t.expand(y.size(0), 1), y], dim=1)
+        return self.g_net(ty).view(y.size(0), self.d, 1)
 
     def h(self, t, y):
-        self._nfe += 1
         return torch.zeros_like(y)
-
-    @property
-    def nfe(self):
-        return self._nfe
 
 
 class Ex3(BaseSDE):
@@ -109,28 +96,30 @@ class Ex3(BaseSDE):
 
     def __init__(self, d, m, sde_type=SDE_TYPES.ito, **kwargs):
         super(Ex3, self).__init__(sde_type=sde_type, noise_type=Ex3.noise_type)
-        self._nfe = 0
+        self.d = d
         self.m = m
 
-        self.a = nn.Parameter(torch.sigmoid(torch.randn(d)), requires_grad=True)
-        self.b = nn.Parameter(torch.sigmoid(torch.randn(d)), requires_grad=True)
+        self.f_net = nn.Sequential(
+            nn.Linear(d + 1, 3),
+            nn.Softplus(),
+            nn.Linear(3, d)
+        )
+        self.g_net = nn.Sequential(
+            nn.Linear(1, 3),
+            nn.Softplus(),
+            nn.Linear(3, d * m),
+            nn.Sigmoid()
+        )
 
     def f(self, t, y):
-        self._nfe += 1
-        return self.b / torch.sqrt(1. + t) - y / (2. + 2. * t)
+        ty = torch.cat([t.expand(y.size(0), 1), y], dim=1)
+        return self.f_net(ty)
 
     def g(self, t, y):
-        self._nfe += 1
-        fill_value = self.a * self.b / torch.sqrt(1. + t)
-        return fill_value.unsqueeze(dim=0).unsqueeze(dim=-1).repeat(y.size(0), 1, self.m)
+        return self.g_net(t.expand(y.size(0), 1)).view(y.size(0), self.d, self.m)
 
     def h(self, t, y):
-        self._nfe += 1
         return torch.zeros_like(y)
-
-    @property
-    def nfe(self):
-        return self._nfe
 
 
 class Ex4(BaseSDE):
@@ -138,7 +127,6 @@ class Ex4(BaseSDE):
 
     def __init__(self, d, m, sde_type=SDE_TYPES.ito, **kwargs):
         super(Ex4, self).__init__(sde_type=sde_type, noise_type=Ex4.noise_type)
-        self._nfe = 0
         self.d = d
         self.m = m
 
@@ -155,22 +143,15 @@ class Ex4(BaseSDE):
         )
 
     def f(self, t, y):
-        self._nfe += 1
-        ty = torch.cat((t.expand_as(y[:, :1]), y), dim=1)
+        ty = torch.cat([t.expand(y.size(0), 1), y], dim=1)
         return self.f_net(ty)
 
     def g(self, t, y):
-        self._nfe += 1
-        ty = torch.cat((t.expand_as(y[:, :1]), y), dim=1)
-        return self.g_net(ty).reshape(-1, self.d, self.m)
+        ty = torch.cat([t.expand(y.size(0), 1), y], dim=1)
+        return self.g_net(ty).reshape(y.size(0), self.d, self.m)
 
     def h(self, t, y):
-        self._nfe += 1
         return torch.zeros_like(y)
-
-    @property
-    def nfe(self):
-        return self._nfe
 
 
 class BasicSDE1(SDEIto):
