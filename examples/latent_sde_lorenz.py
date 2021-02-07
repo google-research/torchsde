d0ba14c88acbba53ee01d7f257aad2c3c6c5c21e
lxuechen <12689993+lxuechen@users.noreply.github.com>
2021-02-07 02:51:53 -0500

    Stochastic Lorenz example.

---
+++ examples/latent_sde_lorenz.py
@@ -0,0 +1,296 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Train a latent SDE on data from a stochastic Lorenz attractor.
+
+Reproduce the toy example in Section 7.2 of https://arxiv.org/pdf/2001.01328.pdf
+
+To run this file, first run the following to install extra requirements:
+
+pip install fire
+
+To run, execute:
+
+python -m examples.latent_sde_lorenz
+"""
+import logging
+import os
+from typing import Sequence
+
+import fire
+import matplotlib.gridspec as gridspec
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import tqdm
+from torch import nn, optim
+from torch.distributions import Normal
+
+import torchsde
+
+
+class LinearScheduler(object):
+    def __init__(self, iters, maxval=1.0):
+        self._iters = max(1, iters)
+        self._val = maxval / self._iters
+        self._maxval = maxval
+
+    def step(self):
+        self._val = min(self._maxval, self._val + self._maxval / self._iters)
+
+    @property
+    def val(self):
+        return self._val
+
+
+class StochasticLorenz(object):
+    """Stochastic Lorenz attractor.
+
+    Used for simulating ground truth and obtaining noisy data.
+    Details described in Section 7.2 https://arxiv.org/pdf/2001.01328.pdf
+    Default a, b from https://openreview.net/pdf?id=HkzRQhR9YX
+    """
+    noise_type = "diagonal"
+    sde_type = "ito"
+
+    def __init__(self, a: Sequence = (10., 28., 8 / 3), b: Sequence = (.1, .28, .3)):
+        super(StochasticLorenz, self).__init__()
+        self.a = a
+        self.b = b
+
+    def f(self, t, y):
+        x1, x2, x3 = torch.split(y, split_size_or_sections=(1, 1, 1), dim=1)
+        a1, a2, a3 = self.a
+
+        f1 = a1 * (x2 - x1)
+        f2 = a2 * x1 - x2 - x1 * x3
+        f3 = x1 * x2 - a3 * x3
+        return torch.cat([f1, f2, f3], dim=1)
+
+    def g(self, t, y):
+        x1, x2, x3 = torch.split(y, split_size_or_sections=(1, 1, 1), dim=1)
+        b1, b2, b3 = self.b
+
+        g1 = x1 * b1
+        g2 = x2 * b2
+        g3 = x3 * b3
+        return torch.cat([g1, g2, g3], dim=1)
+
+    @torch.no_grad()
+    def sample(self, x0, ts, noise_std, normalize):
+        """Sample data for training. Store data normalization constants if necessary."""
+        xs = torchsde.sdeint(self, x0, ts)
+        if normalize:
+            mean, std = torch.mean(xs, dim=(0, 1)), torch.std(xs, dim=(0, 1))
+            xs.sub_(mean).div_(std).add_(torch.randn_like(xs) * noise_std)
+        return xs
+
+
+class Encoder(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(Encoder, self).__init__()
+        self.gru = nn.GRU(input_size=input_size, hidden_size=hidden_size)
+        self.lin = nn.Linear(hidden_size, output_size)
+
+    def forward(self, inp):
+        out, _ = self.gru(inp)
+        out = self.lin(out)
+        return out
+
+
+class LatentSDE(nn.Module):
+    sde_type = "ito"
+    noise_type = "diagonal"
+
+    def __init__(self, data_size, context_size, hidden_size):
+        super(LatentSDE, self).__init__()
+        # Encoder.
+        self.encoder = Encoder(input_size=data_size, hidden_size=hidden_size, output_size=context_size)
+
+        # Decoder.
+        self.f_net = nn.Sequential(
+            nn.Linear(data_size + context_size, hidden_size),
+            nn.Softplus(),
+            nn.Linear(hidden_size, hidden_size),
+            nn.Softplus(),
+            nn.Linear(hidden_size, data_size),
+        )
+        self.h_net = nn.Sequential(
+            nn.Linear(data_size, hidden_size),
+            nn.Softplus(),
+            nn.Linear(hidden_size, hidden_size),
+            nn.Softplus(),
+            nn.Linear(hidden_size, data_size),
+        )
+        self.g_nets = nn.ModuleList(
+            [
+                nn.Sequential(
+                    nn.Linear(1, hidden_size),
+                    nn.Softplus(),
+                    nn.Linear(hidden_size, 1),
+                    nn.Sigmoid()
+                )
+                for _ in range(data_size)
+            ]
+        )
+        self._ctx = None
+
+    def contextualize(self, ctx):
+        self._ctx = ctx  # A tuple of tensors of sizes (T,), (T - 1, batch_size, d).
+
+    def f(self, t, y):
+        ts, ctx = self._ctx
+        i = min(torch.searchsorted(ts, t, right=True), len(ts) - 1)
+        return self.f_net(torch.cat((y, ctx[i]), dim=1))
+
+    def h(self, t, y):
+        return self.h_net(y)
+
+    def g(self, t, y):  # Diagonal diffusion.
+        y = torch.split(y, split_size_or_sections=1, dim=1)
+        out = [g_net_i(y_i) for (g_net_i, y_i) in zip(self.g_nets, y)]
+        return torch.cat(out, dim=1)
+
+    def forward(self, xs, ts, noise_std, adjoint=False, method="euler"):
+        # Contextualization is only needed for posterior inference.
+        ctx = self.encoder(torch.flip(xs, dims=(0,)))
+        ctx = torch.flip(ctx, dims=(0,))
+        self.contextualize((ts, ctx))
+
+        if adjoint:
+            # Must use the argument `adjoint_params`, since `ctx` is not part of the input to `f`, `g`, and `h`.
+            adjoint_params = (
+                    (ctx,) +
+                    tuple(self.f_net.parameters()) + tuple(self.g_nets.parameters()) + tuple(self.h_net.parameters())
+            )
+            _xs, log_ratio = torchsde.sdeint_adjoint(
+                self, xs[0], ts, adjoint_params=adjoint_params, dt=1e-2, logqp=True, method=method)
+        else:
+            _xs, log_ratio = torchsde.sdeint(self, xs[0], ts, dt=1e-2, logqp=True, method=method)
+
+        xs_dist = Normal(loc=_xs, scale=noise_std)
+        log_pxs = xs_dist.log_prob(xs).sum(dim=(0, 2)).mean()
+        log_ratio = log_ratio.sum(dim=0).mean()
+        return log_pxs, log_ratio
+
+    @torch.no_grad()
+    def sample(self, x0, ts, bm=None):
+        return torchsde.sdeint(self, x0, ts, names={'drift': 'h'}, dt=1e-3, bm=bm)
+
+
+def make_dataset(batch_size, noise_std, dump_dir, device):
+    data_path = os.path.join(dump_dir, 'lorenz_data.pth')
+    if os.path.exists(data_path):
+        data_dict = torch.load(data_path)
+        xs, ts = data_dict['xs'], data_dict['ts']
+        logging.warning(f'Loaded toy data at: {data_path}')
+    else:
+        _y0 = torch.randn(batch_size, 3, device=device)
+        ts = torch.linspace(0., 1., steps=50, device=device)
+        xs = StochasticLorenz().sample(_y0, ts, noise_std, normalize=True)
+
+        os.makedirs(os.path.dirname(data_path), exist_ok=True)
+        torch.save({'xs': xs, 'ts': ts}, data_path)
+        logging.warning(f'Stored toy data at: {data_path}')
+    return xs, ts
+
+
+def vis(xs, ts, latent_sde, bm_vis, img_path, num_samples=10):
+    fig = plt.figure(figsize=(20, 9))
+    gs = gridspec.GridSpec(1, 2)
+    ax00 = fig.add_subplot(gs[0, 0], projection='3d')
+    ax01 = fig.add_subplot(gs[0, 1], projection='3d')
+
+    # Left plot: data.
+    z1, z2, z3 = np.split(xs.cpu().numpy(), indices_or_sections=3, axis=-1)
+    [ax00.plot(z1[:, i, 0], z2[:, i, 0], z3[:, i, 0]) for i in range(num_samples)]
+    ax00.scatter(z1[0, :num_samples, 0], z2[0, :num_samples, 0], z3[0, :10, 0], marker='x')
+    ax00.set_yticklabels([])
+    ax00.set_xticklabels([])
+    ax00.set_zticklabels([])
+    ax00.set_xlabel('$z_1$', labelpad=0., fontsize=16)
+    ax00.set_ylabel('$z_2$', labelpad=.5, fontsize=16)
+    ax00.set_zlabel('$z_3$', labelpad=0., horizontalalignment='center', fontsize=16)
+    ax00.set_title('Data', fontsize=20)
+    xlim = ax00.get_xlim()
+    ylim = ax00.get_ylim()
+    zlim = ax00.get_zlim()
+
+    # Right plot: samples from learned model.
+    xs = latent_sde.sample(xs[0], ts, bm=bm_vis).cpu().numpy()
+    z1, z2, z3 = np.split(xs, indices_or_sections=3, axis=-1)
+
+    [ax01.plot(z1[:, i, 0], z2[:, i, 0], z3[:, i, 0]) for i in range(num_samples)]
+    ax01.scatter(z1[0, :num_samples, 0], z2[0, :num_samples, 0], z3[0, :10, 0], marker='x')
+    ax01.set_yticklabels([])
+    ax01.set_xticklabels([])
+    ax01.set_zticklabels([])
+    ax01.set_xlabel('$z_1$', labelpad=0., fontsize=16)
+    ax01.set_ylabel('$z_2$', labelpad=.5, fontsize=16)
+    ax01.set_zlabel('$z_3$', labelpad=0., horizontalalignment='center', fontsize=16)
+    ax01.set_title('Samples', fontsize=20)
+    ax01.set_xlim(xlim)
+    ax01.set_ylim(ylim)
+    ax01.set_zlim(zlim)
+
+    plt.savefig(img_path)
+    plt.close()
+
+
+def main(
+        batch_size=128,
+        context_size=64,
+        hidden_size=128,
+        lr_init=1e-2,
+        lr_gamma=0.999,
+        num_iters=5000,
+        kl_anneal_iters=500,
+        pause_every=50,
+        noise_std=0.01,
+        adjoint=False,
+        dump_dir='./dump/lorenz/',
+        method="euler",
+):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    xs, ts = make_dataset(batch_size=batch_size, noise_std=noise_std, dump_dir=dump_dir, device=device)
+    latent_sde = LatentSDE(data_size=3, context_size=context_size, hidden_size=hidden_size).to(device)
+    optimizer = optim.Adam(params=latent_sde.parameters(), lr=lr_init)
+    scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer=optimizer, gamma=lr_gamma)
+    kl_scheduler = LinearScheduler(iters=kl_anneal_iters)
+
+    # Fix the same Brownian motion for visualization.
+    bm_vis = torchsde.BrownianInterval(size=(batch_size, 3,), device=device, levy_area_approximation="space-time")
+
+    for global_step in tqdm.tqdm(range(1, num_iters + 1)):
+        latent_sde.zero_grad()
+        log_pxs, log_ratio = latent_sde(xs, ts, noise_std, adjoint, method)
+        loss = -log_pxs + log_ratio * kl_scheduler.val
+        loss.backward()
+        optimizer.step()
+        scheduler.step()
+        kl_scheduler.step()
+
+        if global_step % pause_every == 0:
+            lr_now = optimizer.param_groups[0]['lr']
+            logging.warning(
+                f'global_step: {global_step:06d}, lr: {lr_now:.5f}, '
+                f'log_pxs: {log_pxs:.4f}, log_ratio: {log_ratio:.4f} loss: {loss:.4f}, kl_coeff: {kl_scheduler.val:.4f}'
+            )
+            img_path = os.path.join(dump_dir, f'global_step_{global_step:06d}.pdf')
+            vis(xs, ts, latent_sde, bm_vis, img_path)
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
